bacf3b576b5828e6c4c0d14bb8e3b17ff375b7f3
Ayush6589 <ayuyadav0202@gmail.com>
2026-02-05 10:35:58 +0530

    Adding my first java code = Add

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=30;
+        int sum=a+b;
+        System.out.println("Sum is = " + sum);
+    }
+}
